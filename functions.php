c34b00d58253c507825fd6a147b5f7889efae1a6
sotkn <sotkn1@gmail.com>
2025-09-01 14:00:53 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+// Enqueue parent theme styles
+function child_theme_enqueue_styles() {
+    
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    
+    // Enqueue child theme stylesheet
+    wp_enqueue_style( 'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		[ 'parent-style' ]
+	);
+}
+add_action('wp_enqueue_scripts', 'child_theme_enqueue_styles');
+
+
